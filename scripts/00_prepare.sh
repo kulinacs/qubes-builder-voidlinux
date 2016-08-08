a6380ea9bbc8e85c4fa2a4fbda492a3404feb741
Nicklaus McClendon <nicklaus@kulinacs.com>
2016-08-08 18:02:18 -0500

    Completed 00_prepare.sh

--- scripts/00_prepare.sh
+++ scripts/00_prepare.sh
@@ -5,7 +5,7 @@ echo "--> Void Linux 00_prepare.sh"
 
 VOIDLINUX_PLUGIN_DIR="${VOIDLINUX_PLUGIN_DIR:-"${SCRIPTSDIR}/.."}"
 
-VOIDLINUX_SRC_PREFIX="${VOIDLINX_SRC_PREFIX:-http://repo.voidlinux.eu/static}"
+VOIDLINUX_SRC_PREFIX="${VOIDLINX_SRC_PREFIX:-https://repo.voidlinux.eu/static}"
 
 BINARY_TARBALL="xbps-static-latest.x86_64-musl.tar.xz"
 BINARY_CHECKSUM="sha256sums.txt"
@@ -21,20 +21,25 @@ http_proxy="$REPO_PROXY" wget -N -P "$CACHEDIR" "${VOIDLINUX_SRC_PREFIX}/${BINAR
 http_proxy="$REPO_PROXY" wget -N -P "$CACHEDIR" "${VOIDLINUX_SRC_PREFIX}/${BINARY_CHECKSUM}"
 http_proxy="$REPO_PROXY" wget -N -P "$CACHEDIR" "${VOIDLINUX_SRC_PREFIX}/${BINARY_CHECKSUM}.asc"
 
-echo "  --> Preparing GnuPG to verify checksum..."
+echo "  --> Preparing GnuPG to verify checksum signature..."
 mkdir -p "${CACHEDIR}/gpghome"
 chmod -R go-rwx "${CACHEDIR}/gpghome"
-env $GPG_ENV gpg --import "${ARCHLINUX_PLUGIN_DIR}/keys/xtraeme.asc" || exit
+env $GPG_ENV gpg --import "${VOIDLINUX_PLUGIN_DIR}/keys/xtraeme.asc" || exit
 
-echo "  --> Verifying checksum..."
+echo "  --> Verifying checksum signature..."
 env $GPG_ENV gpg --verify "${CACHEDIR}/${BINARY_CHECKSUM}.asc" "${CACHEDIR}/${BINARY_CHECKSUM}" || exit
+
+echo "  --> Verifying Tarball checksum..."
+cd $CACHEDIR
+sha256sum -c "${BINARY_CHECKSUM}" --ignore-missing || exit
+cd -
 
 if [ "${CACHEDIR}/${BINARY_TARBALL}" -nt "${CACHEDIR}/binary/.extracted" ]; then
     echo "  --> Extracting binary tarball (nuking previous directory)..."
     rm -rf "${CACHEDIR}/binary/"
     mkdir -p "${CACHEDIR}/binary"
     # By default will extract to a "root.x86_64" directory; strip that off
-    tar xzC "${CACHEDIR}/binary" -f "${CACHEDIR}/${BINARY_TARBALL}"
+    tar xC "${CACHEDIR}/binary" -f "${CACHEDIR}/${BINARY_TARBALL}"
     # Copy the distribution-provided version to be rewritten based on the
     # value of $PACMAN_MIRROR each run (by the Makefile)
     #cp -a "${CACHEDIR}/bootstrap/etc/pacman.d/mirrorlist" "${CACHEDIR}/bootstrap/etc/pacman.d/mirrorlist.dist"
